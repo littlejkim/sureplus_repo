9e4d8d3f17dd929608a4819cb3a81e01ac0b6907
youngmi97 <jaehwan@sureplus.io>
2021-04-02 00:11:43 +0900

    admin usergroup

--- amplify/backend/function/twiliosms/src/app.js
+++ amplify/backend/function/twiliosms/src/app.js
@@ -12,6 +12,20 @@ var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware'
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 var CryptoJS = require('crypto-js');
 
+//configuration for admin gql queries and mutation
+const aws = require('aws-sdk');
+const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider({
+  apiVersion: '2016-04-18',
+});
+const cognitoSP = new aws.CognitoIdentityServiceProvider({
+  region: process.env.REGION,
+});
+
+//const AWSAppSyncClient = require('aws-appsync').default;
+
+let cred;
+let credExpirationDate = new Date('01-01-1970'); // to keep track of if credentials are out of date
+
 // declare a new express app
 var app = express();
 app.use(urlencoded({ extended: false }));
@@ -27,6 +41,68 @@ app.use(function (req, res, next) {
 /****************************
  * Example post method *
  ****************************/
+
+//1. Configure to access the gql mutation
+//2. call createDeviceItem mutation from lambda
+//3. client is subscribed to the mutation event. If the deviceId is the same, it will take action
+
+const initiateAuthParams = {
+  AuthFlow: 'ADMIN_NO_SRP_AUTH',
+  ClientId: process.env.COGNITO_CLIENT_ID, // use env variables or SSM parameters
+  UserPoolId: process.env.COGNITO_USER_POOL_ID, // use env variables or SSM parameters
+  AuthParameters: {
+    USERNAME: process.env.ADMIN_USER_USERNAME, // use env variables or SSM parameters
+    PASSWORD: process.env.ADMIN_USER_PASSWORD, // use env variables or SSM parameters
+  },
+};
+
+// Get an Id Token (JWT) for the user
+function getCredentials() {
+  return new Promise((resolve, reject) => {
+    cognitoSP.adminInitiateAuth(initiateAuthParams, (authErr, authData) => {
+      if (authErr) {
+        console.log(authErr);
+        reject(authErr);
+      } else if (authData === null) {
+        reject('Auth data is null');
+      } else {
+        console.log('Auth Successful');
+        resolve(authData);
+      }
+    });
+  });
+}
+
+// function getAppSyncClient() {
+//   return new AWSAppSyncClient({
+//     disableOffline: true,
+//     url:
+//       'https://4t3zqgal2bfhriegqziwd3ft24.appsync-api.us-east-2.amazonaws.com/graphql',
+//     region: process.env.REGION,
+//     auth: {
+//       type: 'AMAZON_COGNITO_USER_POOLS',
+//       jwtToken: async () => {
+//         // check if we already have credentials or if credentials are expired
+//         if (!cred || credExpirationDate < new Date()) {
+//           // get new credentials
+//           cred = await getCredentials();
+//           // give ourselves a 10 minute leeway here
+//           credExpirationDate = new Date(
+//             +new Date() + (cred.AuthenticationResult.ExpiresIn - 600) * 1000,
+//           );
+//         }
+//         return cred.AuthenticationResult.IdToken;
+//       },
+//     },
+//   });
+// }
+
+// Handling Requests
+
+app.post('/update/devicelist', (req, res) => {
+  console.log('FUNCTION CALLED!', req.body);
+  res.json({ verified: true });
+});
 
 const encryptionKey = 'jioy7A!Y&h9ha90AJkJA872';
 app.post('/sms', (req, res) => {
